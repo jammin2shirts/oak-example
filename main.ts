e9ce83467505a863cbc186b5c5bd6ceb701a1b2e
jammin2shirts <teuchertb@gmail.com>
2023-04-27 13:55:40 -0400

    removed books

--- main.ts
+++ main.ts
@@ -1,24 +1,9 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
-const books = new Map<string, any>();
-books.set("1", {
-  id: "1",
-  title: "The Hound of the Baskervilles",
-  author: "Conan Doyle, Arthur",
-});
-
 const router = new Router();
 router
   .get("/", (context) => {
     context.response.body = "Hello world!";
-  })
-  .get("/book", (context) => {
-    context.response.body = Array.from(books.values());
-  })
-  .get("/book/:id", (context) => {
-    if (books.has(context?.params?.id)) {
-      context.response.body = books.get(context.params.id);
-    }
   })
   .get("/elections/:county", async (context) => {
     context.response.body = await (await fetch(`https://www.voterfocus.com/CampaignFinance/candidate_pr.php?c=`+context?.params?.county)).text();
