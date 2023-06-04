e8cd3463963eba03c902c4a591f3eda2516bbbaa
jammin2shirts <teuchertb@gmail.com>
2023-06-03 20:47:23 -0400

    testing changes

--- main.ts
+++ main.ts
@@ -1,44 +1,63 @@
-// import { Application, Router, } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
-// const router = new Router();
-// router
-//   .get("/", (context) => {
-//     context.response.body = "Hello world!";
-//   })
-//   .get("/elections/:county", async (context) => {
-//     context.response.body = await (await fetch(`https://www.voterfocus.com/CampaignFinance/candidate_pr.php?c=`+context?.params?.county)).text();
-//   })
-//   .get("/wss", (ctx) => {
-//     if (!ctx.isUpgradable) {
-//       ctx.throw(501);
-//     }
-//     const ws = ctx.upgrade();
-//     ws.onopen = () => {
-//       console.log("Connected to client");
-//       ws.send("Hello from server!");
-//     };
-//     ws.onmessage = (m) => {
-//       console.log("Got message from client: ", m.data);
-//       ws.send(m.data as string);
-//       ws.close();
-//     };
-//     ws.onclose = () => console.log("Disconncted from client");
-//   });
+const availableRooms: string[] = ["wow"];
+const addNewRoom = (name: string) => {
+  availableRooms.push(name);
+};
 
-// const app = new Application();
-// app.use(router.routes());
-// app.use(router.allowedMethods());
-
-// await app.listen({ port: 8000 });
-
-import { WebSocketClient, WebSocketServer } from "https://deno.land/x/websocket@v0.1.4/mod.ts";
-
-const wss = new WebSocketServer(8000);
-wss.on("connection", function (ws: WebSocketClient) {
-  console.dir(ws)
-  console.log('connected')
-  ws.on("message", function (message: string) {
-    console.log(message);
-    ws.send(message);
+const respondWithAllRooms = (ws: WebSocket) => {
+  const refreshedRooms = {
+    name: "All Rooms",
+    action: "ShowRooms",
+    value: availableRooms,
+  };
+  const openBlob = new Blob([JSON.stringify(refreshedRooms, null, 2)], {
+    type: "application/json",
   });
-});
+  ws.send(openBlob);
+};
+const router = new Router();
+router
+  .get("/wss", (ctx) => {
+    if (!ctx.isUpgradable) {
+      ctx.throw(501);
+    }
+    const ws = ctx.upgrade();
+
+    ws.onopen = () => {
+      console.log("Connected to client");
+      // ws.send("Hello from server!");
+      respondWithAllRooms(ws);
+    };
+
+    ws.onmessage = async ({ data }) => {
+      const blob = new Blob([data]);
+      const { name, action } = JSON.parse(await blob.text());
+      if (action == "AddRoom") {
+        addNewRoom(name);
+        respondWithAllRooms(ws);
+      }
+      console.log(`Got message from client:${name} - ${action}`);
+
+      ws.send("got the message");
+    };
+
+    ws.onclose = () => console.log("Disconncted from client");
+  })
+  .get("/wss/rooms", (ctx) => {
+    //use this to programmatically set new websockets for each new room added
+    ctx.response.body = availableRooms;
+  })
+  .get("/wss/:roomName", (ctx) => {
+    //use this to programmatically set new websockets for each new room added
+    if (availableRooms.includes(ctx?.params?.roomName)) {
+      console.log("this room exists");
+      ctx.response.body = `it's here`;
+    }
+  });
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
